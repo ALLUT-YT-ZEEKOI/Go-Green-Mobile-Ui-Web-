d6152d471a34111d3f3cbabf7b1cfb6d38717dee
unknown <ameenofficial33@gmail.com>
2024-11-19 23:37:03 +0530

    p-s

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let currentSlideIndex = 0;
 const carouselContent = document.getElementById("courcel_content_z1");
 const dots = document.querySelectorAll(".dot_z1");
-
 function updateCarousel() {
   const offset = -currentSlideIndex * 100; // Offset for sliding effect
   carouselContent.style.transform = `translateX(${offset}%)`;
@@ -11,6 +10,7 @@ function updateCarousel() {
     dot_z1.classList.toggle("active", index === currentSlideIndex);
   });
 }
+
 
 function nextSlide() {
   currentSlideIndex = (currentSlideIndex + 1) % dots.length;
