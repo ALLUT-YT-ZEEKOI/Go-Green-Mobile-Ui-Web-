3f397aa8df7a53772b627ef91317928b11c236a1
Ameen.s <157443572+ALLUT-YT-ZEEKOI@users.noreply.github.com>
2024-11-21 12:34:04 +0530

    firsttt

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let currentSlideIndex = 0;
 const carouselContent = document.getElementById("courcel_content_z1");
-const dots = document.querySelectorAll(".dot_z1");
+const dots = document.querySelectorAll(".dot");
+
 function updateCarousel() {
   const offset = -currentSlideIndex * 100; // Offset for sliding effect
   carouselContent.style.transform = `translateX(${offset}%)`;
 
   // Update active dot
-  dots.forEach((dot_z1, index) => {
-    dot_z1.classList.toggle("active", index === currentSlideIndex);
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("active", index === currentSlideIndex);
   });
 }
-
 
 function nextSlide() {
   currentSlideIndex = (currentSlideIndex + 1) % dots.length;
@@ -21,8 +21,6 @@ function setCurrentSlide(index) {
   currentSlideIndex = index;
   updateCarousel();
 }
-
-// Auto-slide every 3 seconds
 setInterval(nextSlide, 3000);
 
 //  <!-- Direct From Farm category-->
@@ -39,11 +37,10 @@ for (let i = 0; i <= 5; i++) {
 }
 category_main_boxz1.remove();
 
-
 // Farm fresh picks loop
 const home_parent_card = document.getElementById("home_parent_card_z1");
 const home_child_card = document.getElementById("home_child_card_z1");
-for(let i = 0 ;i<=5;i++){
+for (let i = 0; i <= 5; i++) {
   const clone = home_child_card.cloneNode(true);
   home_parent_card.appendChild(clone);
 }
